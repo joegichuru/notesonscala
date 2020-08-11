612fa220eab85c1013565fd72e39831d3013ea8c
joe gichuru <joegichuru@outlook.com>
2020-08-11 12:31:24 +0300

    initial

---
+++ src/com/joseph/Variables.scala
@@ -0,0 +1,8 @@
+package com.joseph
+
+class Variables {
+  var number=100//number will be mutable i.e it can be changed
+  val pi=3.142 //pi will be immutable hence cannot be re assigned
+  //pi=900//wont compile
+  var n:Int=200//type annotation
+}
---
+++ src/com/joseph/package-info.java
@@ -0,0 +1,5 @@
+package com.joseph;
+/*
+ * These are series of code snippets that i use to explore various Scala language features.
+ *
+ **/
